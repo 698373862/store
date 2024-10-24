215f63c5275422ed2057fe99949645ef841ea51b
dawid <dawid.kikosicki@hotmail.com>
2024-10-24 09:54:35 +0200

    init

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { useWebsiteStore } from '~/stores/websiteStore';
+
+const store = useWebsiteStore()
+const counter = useState("counter", () => 0);
+</script>
+
+<template>
+  state test
+  counter: {{ counter }}
+  <button @click="counter++">Inc</button>
+
+  store test:
+    {{  store.name }}
+</template>
---
+++ pages/subpage.vue
@@ -0,0 +1,3 @@
+<template>
+    subpage
+</template>
